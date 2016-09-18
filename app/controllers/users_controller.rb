6280dfe45bb44dc638a17a5a31a7f4f4f944c74d
amritakc <amritakc@gmail.com>
2016-09-17 17:22:13 -0700

    changed database username

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,7 +45,7 @@ class UsersController < ApplicationController
 
 	private
 	def user_params
-		params.require(:user).permit(:name, :alias, :email, :password, :password_confirmation)
+		params.require(:user).permit(:name, :alias, :email, :password, :pwd)
 	end
 
 end
